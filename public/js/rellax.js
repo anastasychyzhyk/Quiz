4d3a77fcf7947525bc4673aa1bc50e786706aef4
aliceFrontend <alice.goncharova2018@yandex.ru>
2020-09-03 20:33:40 +0300

    Creating a home page
    Add a Home controller, change the base template

---
+++ public/js/rellax.js
@@ -0,0 +1,11 @@
+ //var rellax = new Rellax('.rellax', {
+   // speed: -4,
+   // center: false,
+   // round: true,
+ //});
+
+if (window.matchMedia("(min-width: 400px)").matches) {
+    /* the viewport is at least 400 pixels wide */
+} else {
+    /* the viewport is less than 400 pixels wide */
+}
