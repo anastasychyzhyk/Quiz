7e7611f4112cf4979397d116523d71f40f702b0b
Anastasy Chizhik <katenok-nastja@mail.ru>
2020-09-15 15:24:23 +0300

    RestorePassword fixing PasswordType

--- src/Form/RestorePasswordType.php
+++ src/Form/RestorePasswordType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,9 +13,11 @@ class RestorePasswordType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder
-            ->add('password')
-        ;
+        $builder->add('password', RepeatedType::class, [
+            'type' => PasswordType::class,
+            'first_options' => ['label' => 'Password'],
+            'second_options' => ['label' => 'Confirm Password']
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
